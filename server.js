3cce88a27839f66a904e9355a2ac46596fa4f2f8
0magical <marilynventura@outlook.com>
2022-10-28 14:51:41 -0400

    added uid and other things to server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const { text } = require('express')
 const express = require('express')
+const {uid} = require('uid')
+let notes = require('./db/db.json')
 
 const app = express()
+
+const path = require ('path')
+const { title } = require('process')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -8,9 +14,33 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
 
+app.post('/api/notes', (req, res) =>{
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uid(),
+    }
+
+    notes.push(newNote)
+    res.json(200)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    notes = notes.filter( note => note.id !== req.params.id )
+    res.json(notes)
+})
 
 
 
